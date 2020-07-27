1e4b1e1e71ef8130849e0c2d7f97bb3734f7d315
rahman9868 <rahman9868@gmail.com>
2020-07-27 09:43:11 +0700

    HILT version 0.2
    Enhancement :
    - Field and Constructor Injection

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,12 +2,48 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    //field injection
+    @Inject
+    lateinit var someClass: SomeClass
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        println(someClass.doAThing())
+        println(someClass.doSomeOtherThing())
 
+    }
+}
+
+class SomeClass
+@Inject
+constructor(
+    //Constructor Injection
+    private val someOtherClass: SomeOtherClass
+){
+
+
+    fun doAThing(): String{
+        return "Look I did a thing"
+    }
+
+    fun doSomeOtherThing(): String{
+        return someOtherClass.doSOmeOtherThing()
+    }
+}
+
+class SomeOtherClass
+@Inject
+constructor(){
+
+    fun doSOmeOtherThing(): String{
+        return "Look I did some other thing!"
     }
 }
