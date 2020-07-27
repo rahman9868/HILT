f8a5b4d0b9a2d152834ba285089e91cc7177f9f2
rahman9868 <rahman9868@gmail.com>
2020-07-27 12:40:22 +0700

    HILT version 0.3
    Enhancement :
    - Scoping with HILT

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,8 +2,12 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.scopes.ActivityScoped
+import dagger.hilt.android.scopes.FragmentScoped
 import javax.inject.Inject
+import javax.inject.Singleton
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -17,16 +21,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println(someClass.doAThing())
-        println(someClass.doSomeOtherThing())
 
     }
 }
 
+class MyFragment: Fragment(){
+
+    @Inject
+    lateinit var someClass: SomeClass
+}
+
+
+//
+//@FragmentScoped -> Jika Menggunakan ini akan error di class MainActivity karena Acivity tidak bisa Menginject Fragment , tp sebalikanya Fragment bisa menginject Activity, seperti pada class MyFragment
+//https://developer.android.com/training/dependency-injection/hilt-android#component-scopes
+
+@ActivityScoped
 class SomeClass
 @Inject
 constructor(
-    //Constructor Injection
-    private val someOtherClass: SomeOtherClass
 ){
 
 
@@ -34,9 +47,6 @@ constructor(
         return "Look I did a thing"
     }
 
-    fun doSomeOtherThing(): String{
-        return someOtherClass.doSOmeOtherThing()
-    }
 }
 
 class SomeOtherClass
